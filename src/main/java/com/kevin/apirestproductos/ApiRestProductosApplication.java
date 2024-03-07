68fd3aa9632384e936b19f68ba913f0a37396f3c
kevin acosta <kevinacosta808@gmail.com>
2024-03-07 15:05:27 -0500

    commit initial

---
+++ src/main/java/com/kevin/apirestproductos/ApiRestProductosApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.apirestproductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestProductosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevin/apirestproductos/ApiRestProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevin.apirestproductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
